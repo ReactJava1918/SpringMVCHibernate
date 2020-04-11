d7e5e6d30504dd401bea709d09ccc4cbb95d9e82
siva <reddy.sivareddy2@gmail.com>
2020-04-11 15:35:40 +0530

    added test classess

---
+++ src/test/java/TestAssertions.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestAssertions {
+
+   @Test
+   public void testAssertions() {
+      //test data
+      
+		
+      int val1 = 6;
+      int val2 = 6;
+
+      
+
+      //Check that two objects are equal
+      assertEquals(val1, val2);
+
+             
+   }
+}
